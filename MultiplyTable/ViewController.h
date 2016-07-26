2a44ae0da5e7241662a306f526cfb2e2df97c376
Student-003 <student-003@fexli-itss-macbook-pro.local>
2016-07-26 09:22:49 +0530

    Initial Commit

---
+++ MultiplyTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiplyTable
+//
+//  Created by Student-003 on 26/07/16.
+//  Copyright © 2016 Felix-ITs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MultiplyTable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MultiplyTable
+//
+//  Created by Student-003 on 26/07/16.
+//  Copyright © 2016 Felix-ITs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
